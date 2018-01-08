6e63f4d6ac6ffd4bb71f183411bf2808aeea4f8a
tz1006 <a@omg.tf>
2018-01-07 20:49:13 -0800

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*- 
+# filename: sms.py
+
+
+################
+
+import nexmo
+
+key = '3ab7e80a'
+secret = '29e759265059bb18'
+
+client = nexmo.Client(key=key, secret=secret)
+
+def send_sms(number, message):
+  client.send_message({
+    'from': 12015946703,
+    'to': number,
+    'text': message,
+  })
+
+phone = 16267318573
+text = 'A text message sent using the Nexmo SMS API'
+#send_sms(phone, text)
+
