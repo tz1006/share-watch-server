6b6868bd62886e60a9c0dbdc8f33615a814d9857
tz1006 <a@omg.tf>
2018-01-09 22:39:05 -0800

    Create main_t1.py

---
+++ main_t1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*- 
+# filename: main_t1.py
+
+from sharelist_t import share_list
+import sort_t
+
+share_list = sort_t.sort_list(share_list)
+last_ma = sort_t.last_ma
+
+from monitor1 import *
+ma_monitor_start(share_list, last_ma)
+
+
+import code
+code.interact(banner = "", local = locals())
